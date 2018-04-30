a065f09c3adceec78b95c91d52e2dc1ad1609ddb
Jerry Jiahao Xu <jerryhuimanutd@gmail.com>
2018-04-30 18:16:38 -0400

    add lineplot()

--- visualization.py
+++ visualization.py
@@ -17,3 +17,20 @@ def scatterplot(x_data, y_data, x_label='', y_label='', title='', color='r', ysc
     ax.set_ylabel(y_label)
 
     plt.show()
+
+def lineplot(x_data, y_data, x_label='', y_label='', title='', color='r', yscale_log=False):
+    # create the plot object
+    _, ax = plt.subplots()
+
+    # plot the data, set the size, color and transparancy (alpha)
+    ax.plot(x_data, y_data, s=10, color=color, alpha=0.75)
+
+    if yscale_log:
+        ax.set_yscale('log')
+
+    # labels and title
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+
+    plt.show()
