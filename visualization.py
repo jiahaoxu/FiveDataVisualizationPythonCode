9dce7d89389cfbeab9314712e3c24969e4c087a6
Jerry Jiahao Xu <jerryhuimanutd@gmail.com>
2018-04-30 18:02:18 -0400

    initial scatter plot

---
+++ visualization.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def scatterplot(x_data, y_data, x_label='', title='', color='r', yscale_log=False):
+    # create the plot object
+    _, ax = plt.subplots()
+
+    # plot the data, set the size, color and transparancy (alpha)
+    ax.scatterplot(x_data, y_data, s=10, color=color, alpha=0.75)
+
+    if yscale_log:
+        ax.set_yscale('log')
+
+    # labels and title
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
